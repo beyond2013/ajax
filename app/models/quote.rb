30a40f743f597678d5d71ca22f7c4f7d73f2de7b
Imran Ali <imran.shahjee@gmail.com>
2014-10-16 11:01:51 +0700

    quote model validations added

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,2 +1,6 @@
 class Quote < ActiveRecord::Base
+	CONTENT_MIN_LENGTH = 15
+	CONTENT_MAX_LENGTH = 255
+	CONTENT_RANGE = CONTENT_MIN_LENGTH..CONTENT_MAX_LENGTH
+	validates_length_of :content, :within => CONTENT_RANGE
 end
